69c7c7939643e7cdbb5fb3ec2b9f4187f6aa5d75
Zaikin Alex <aa.petrony@gmail.com>
2026-07-01 10:15:54 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-const gulp = require('gulp');
-const exec = require('gulp-exec');
-const path = require('path');
+const gulp = require("gulp");
+const exec = require("gulp-exec");
+const path = require("path");
 
-gulp.task('compress-video', () => {
-  const inputDir = './source-videos/*.mp4'; // Вихідна папка з відео
-  const outputDir = './compressed-videos/'; // Папка для стиснутих відео
+gulp.task("compress-video", () => {
+  const inputDir = "./source-videos/*.mp4"; // Output folder for videos
+  const outputDir = "./compressed-videos/"; // Folder for compressed videos
 
-  return gulp.src(inputDir)
-    .pipe(exec(file => {
-      const fileName = path.basename(file.path, path.extname(file.path));
-      const outputPath = `${outputDir}${fileName}.mp4`;
-      return `ffmpeg -i ${file.path} -vcodec libx264 -crf 28 -preset faster ${outputPath}`;
-    }))
+  return gulp
+    .src(inputDir)
+    .pipe(
+      exec((file) => {
+        const fileName = path.basename(file.path, path.extname(file.path));
+        const outputPath = `${outputDir}${fileName}.mp4`;
+        return `ffmpeg -i ${file.path} -vcodec libx264 -crf 28 -preset faster ${outputPath}`;
+      }),
+    )
     .pipe(exec.reporter());
 });
 
-gulp.task('default', gulp.series('compress-video'));
+gulp.task("default", gulp.series("compress-video"));
